c78f3948c724d1aa02207f7b5622ec7fb5226120
oksana.repkina <oksana.repkina@nakitel.com>
2017-09-18 18:02:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony-blog
+============
+
+A Symfony project created on September 14, 2017, 10:45 am.
---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oksana.repkina
+ * Date: 18.09.17
+ * Time: 15:21
+ */
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        $comments = $em->getRepository('BloggerBlogBundle:Comment')
+            ->getCommentsForBlog($blog->getId());
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+            'comments'  => $comments
+        ));
+    }
+}
